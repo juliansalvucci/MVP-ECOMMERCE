f46dee5cba2df848b0b30baf203ec3217601c494
Julian López Salvucci <julian.lopez@asociart.com.ar>
2023-12-09 14:11:58 -0300

    categoria

--- BACKEND/ECOMMERCE/ECOMMERCE/Models/Producto.cs
+++ BACKEND/ECOMMERCE/ECOMMERCE/Models/Producto.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string NombreProducto { get; set; }
+        public decimal Precio { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
---
+++ BACKEND/ECOMMERCE/ECOMMERCE/Models/SubCategoria.cs
@@ -0,0 +1,10 @@
+﻿namespace ECOMMERCE.Models
+{
+    public class SubCategoria
+    {
+        public int Id { get; set; }
+        public string NombreSubCategoria { get; set; } = "";
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; } = new Categoria();
+    }
+}
