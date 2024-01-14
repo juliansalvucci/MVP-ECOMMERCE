6434dd54e58e961c1e37705ffe3b8439328b4ca5
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-01-14 18:04:02 -0300

    .

--- FRONTEND/ECOMMERCE/src/components/Producto.jsx
+++ FRONTEND/ECOMMERCE/src/components/Producto.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
+
 export function Producto(Producto) {
+
+  const [productoCarrito, setProductoCarrito] = useState([]);
+
+  const agregarACarrito = async () => {
+    localStorage.setItem(localStorageKey, datos);
+  }
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, datos);
+  }, [datos]);
+
   return (
     <div className="text-center p-5">
       <img src="./assets/sin-imagen.png" alt="" className="img-fluid rounded-pill" />
       <p>{`Artículo: ${Producto.nombreProducto}`}</p>
       <p>{`Precio: $ ${Producto.precio}`}</p>
-      <button onClick={()=> console.log(Producto)}>SELECCIONAR</button>
+      <button onClick={() => console.log(Producto)}>SELECCIONAR</button>
     </div>
   );
 }
