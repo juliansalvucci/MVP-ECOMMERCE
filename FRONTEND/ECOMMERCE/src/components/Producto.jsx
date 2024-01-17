42cc2177f460fc700d8f28ba3effe89798b42dc3
Julian López Salvucci <julian.lopez@asociart.com.ar>
2024-01-16 21:54:46 -0300

    .

--- FRONTEND/ECOMMERCE/src/components/Producto.jsx
+++ FRONTEND/ECOMMERCE/src/components/Producto.jsx
@@ -1,23 +1,26 @@
 import { useState, useEffect } from "react";
 
 export function Producto(Producto) {
+  const [productosCarrito, setProductosCarrito] = useState([]);
 
-  const [productoCarrito, setProductoCarrito] = useState([]);
-
-  const agregarACarrito = async () => {
-    localStorage.setItem(localStorageKey, datos);
-  }
+  const agregarACarrito = async (producto) => {
+    setProductosCarrito(producto);
+  };
 
   useEffect(() => {
-    localStorage.setItem(localStorageKey, datos);
-  }, [datos]);
+    localStorage.setItem("carrito", productosCarrito);
+  }, [productosCarrito]);
 
   return (
     <div className="text-center p-5">
-      <img src="./assets/sin-imagen.png" alt="" className="img-fluid rounded-pill" />
+      <img
+        src="./assets/sin-imagen.png"
+        alt=""
+        className="img-fluid rounded-pill"
+      />
       <p>{`Artículo: ${Producto.nombreProducto}`}</p>
       <p>{`Precio: $ ${Producto.precio}`}</p>
-      <button onClick={() => console.log(Producto)}>SELECCIONAR</button>
+      <button onClick={() => agregarACarrito(Producto)}>SELECCIONAR</button>
     </div>
   );
 }
