e365228d8bdb46ef45e3427be2ada07b79a59c72
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2024-01-28 10:56:41 -0300

    prop imagen

---
+++ BACKEND/ECOMMERCE/ECOMMERCE/Migrations/20240128134950_imageb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECOMMERCE.Migrations
+{
+    public partial class imageb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Imagen",
+                table: "Producto",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imagen",
+                table: "Producto");
+        }
+    }
+}
--- BACKEND/ECOMMERCE/ECOMMERCE/Models/Producto.cs
+++ BACKEND/ECOMMERCE/ECOMMERCE/Models/Producto.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string NombreProducto { get; set; }
         public decimal Precio { get; set; }
-        //public Categoria Categoria { get; set; }
+        public byte[] Imagen { get; set; }
     }
 }
